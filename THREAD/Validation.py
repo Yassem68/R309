9580c61c3d22cc939684d8c6f6b3ba65cfc00aad
Yassem68 <yassem.mohareb@uha.fr>
2022-11-17 09:50:32 +0100

    .

---
+++ THREAD/Validation.py
@@ -0,0 +1,8 @@
+import threading
+import time
+import concurrent.futures
+import multiprocessing
+import requests
+
+img_urls = ['https://i.pinimg.com/564x/9b/cd/98/9bcd986b312668ddfec3202d22b073d9.jpg','https://i.pinimg.com/564x/48/30/6f/48306fb784cb718538efaff84be08dcd.jpg',
+'https://i.pinimg.com/564x/d4/02/52/d4025245b5f8f188364f6af366ef4aab.jpg']
