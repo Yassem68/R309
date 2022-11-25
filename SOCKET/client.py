a4d96a7cea83957e53c550fbc5713f2c05b5a2c0
Yassem68 <yassem.mohareb@uha.fr>
2022-11-25 14:03:03 +0100

    .

--- SOCKET/client.py
+++ SOCKET/client.py
@@ -4,8 +4,9 @@ import time
 import socketserver
 
 client_socket = socket.socket()
-client_socket.connect(('localhost', 5000))
+client_socket.connect(('localhost', 6000))
 client_socket.send('Hello World'.encode())
 data = client_socket.recv(1024).decode()
 print(data)
+
 client_socket.close()
--- SOCKET/server.py
+++ SOCKET/server.py
@@ -4,7 +4,7 @@ import time
 import socketserver
 
 server_socket = socket.socket()
-server_socket.bind(('10.171.251.68', 6000))
+server_socket.bind(('localhost', 6000))
 server_socket.listen(5)
 print("Serveur en écoute")
 conn, addr = server_socket.accept()
@@ -12,4 +12,5 @@ conn, addr = server_socket.accept()
 data = conn.recv(1024).decode()
 conn.send(b'CA VA OU QUOI ?')
 print(data)
+print ("Le serveur se ferme")
 conn.close()
